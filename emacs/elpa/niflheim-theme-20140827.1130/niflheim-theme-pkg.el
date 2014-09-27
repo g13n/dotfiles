a3133179e7235426e2528d3a1fc97f947d606f49
Gopal Venkatesan <gv@g13n.me>
2014-09-26 17:51:28 -0700

    Use a default dark theme

---
+++ emacs/elpa/niflheim-theme-20140827.1130/niflheim-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "niflheim-theme" "20140827.1130" "A port of the Nifleim theme to Emacs" (quote nil))
--- emacs/init.el
+++ emacs/init.el
@@ -309,7 +309,7 @@
       (add-to-list 'custom-theme-load-path (expand-file-name "~/src/build/tomorrow-theme/GNU Emacs"))
       (add-to-list 'custom-theme-load-path (expand-file-name "~/.emacs.d/themes/color-theme-radiance"))
       ))
-;(load-theme 'solarized-light)
+(load-theme 'niflheim)
 
 ;; RCIRC
 (require 'tls)
@@ -334,7 +334,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("e3a3b7d7fe89b5d57d40bc825ca2324875a6f37bd63da66f2a6fc68cc8b2ee95" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "0795e2c85394140788d72d34969be4acb305e4a54149e7237787d9df27832fbb" "53c542b560d232436e14619d058f81434d6bbcdc42e00a4db53d2667d841702e" "bd115791a5ac6058164193164fd1245ac9dc97207783eae036f0bfc9ad9670e0" "a99e7c91236b2aba4cd374080c73f390c55173c5a1b4ac662eeb3172b60a9814" "fc2782b33667eb932e4ffe9dac475f898bf7c656f8ba60e2276704fabb7fa63b" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "60f04e478dedc16397353fb9f33f0d895ea3dab4f581307fbf0aa2f07e658a40" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3c708b84612872e720796ea1b069cf3c8b3e909a2e1da04131f40e307605b7f9" "572caef0c27b100a404db8d540fd5b31397f90ab660ef5539ff0863ff9bee26a" default))))
+ '(custom-safe-themes (quote ("d25594db11e666e3d670c739266f62b206814e261e8548becef5258d6cfbd50b" "e3a3b7d7fe89b5d57d40bc825ca2324875a6f37bd63da66f2a6fc68cc8b2ee95" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "0795e2c85394140788d72d34969be4acb305e4a54149e7237787d9df27832fbb" "53c542b560d232436e14619d058f81434d6bbcdc42e00a4db53d2667d841702e" "bd115791a5ac6058164193164fd1245ac9dc97207783eae036f0bfc9ad9670e0" "a99e7c91236b2aba4cd374080c73f390c55173c5a1b4ac662eeb3172b60a9814" "fc2782b33667eb932e4ffe9dac475f898bf7c656f8ba60e2276704fabb7fa63b" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "60f04e478dedc16397353fb9f33f0d895ea3dab4f581307fbf0aa2f07e658a40" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3c708b84612872e720796ea1b069cf3c8b3e909a2e1da04131f40e307605b7f9" "572caef0c27b100a404db8d540fd5b31397f90ab660ef5539ff0863ff9bee26a" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
